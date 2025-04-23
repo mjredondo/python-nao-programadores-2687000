18f776b3a3570b839dc49cb22bf5ba4439109880
mjredondo <mario_redondo28@hotmail.com>
2025-04-23 13:19:32 +0000

    Criação de Variáveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,12 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+username = 'Mário'
 
 # Declare variáveis de cada tipo numérico
-
+ano = 2025
+salario = 12.3
 
 # Declare uma variável do tipo booleano
+Verdadeiro = True
+Falso = False
